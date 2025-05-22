65bd39f3bbe898cdbd7eea1b53acac3bd474af6a
Facundo <facundoarueda@hotmail.com>
2025-05-22 15:58:13 -0300

    Format code

--- src/modules/users/dto/user-dto.ts
+++ src/modules/users/dto/user-dto.ts
@@ -1,47 +1,46 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { Type } from "class-transformer";
-import { IsDate, IsNotEmpty, IsNumber, IsString } from "class-validator";
+import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import { IsDate, IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class UserDto {
+  @ApiProperty({
+    name: 'id',
+    type: Number,
+    description: 'Identificador del usuario',
+    required: true,
+  })
+  @IsNumber()
+  @IsNotEmpty()
+  id: number;
 
-    @ApiProperty({
-        name: 'id',
-        type: Number,
-        description: 'Identificador del usuario',
-        required: true
-    })
-    @IsNumber()
-    @IsNotEmpty()
-    id: number;
+  @ApiProperty({
+    name: 'name',
+    type: String,
+    description: 'Nombre del usuario',
+    required: true,
+  })
+  @IsString()
+  @IsNotEmpty()
+  name: string;
 
-    @ApiProperty({
-        name: 'name',
-        type: String,
-        description: 'Nombre del usuario',
-        required: true
-    })
-    @IsString()
-    @IsNotEmpty()
-    name: string;
+  @ApiProperty({
+    name: 'email',
+    type: String,
+    description: 'Email del usuario',
+    required: true,
+  })
+  @IsString()
+  @IsNotEmpty()
+  email: string;
 
-    @ApiProperty({
-        name: 'email',
-        type: String,
-        description: 'Email del usuario',
-        required: true
-    })
-    @IsString()
-    @IsNotEmpty()
-    email: string;
-
-    @ApiProperty({
-        name: 'birthDate',
-        type: Date,
-        description: 'Fecha de nacimiento del usuario',
-        required: true
-    })
-    @IsDate()
-    @IsNotEmpty()
-    @Type(() => Date)
-    birthDate: Date;
+  @ApiProperty({
+    name: 'birthDate',
+    type: Date,
+    description: 'Fecha de nacimiento del usuario',
+    required: true,
+  })
+  @IsDate()
+  @IsNotEmpty()
+  @Type(() => Date)
+  birthDate: Date;
 }
